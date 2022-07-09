e4a44293a211c1fa25018518b3a3abdfec12e262
Jaslan <Jaslan.dlnv@gmail.com>
2022-07-09 19:27:43 +0500

    feat: add assets files

---
+++ online-store/src/core/ts/declaration.d.ts
@@ -0,0 +1,11 @@
+declare module '*.jpg' {
+  const content: string;
+  export = content;
+}
+declare module '*.png';
+declare module '*.jpeg';
+declare module '*.webp';
+declare module '*.gif';
+declare module '@imgs';
+declare module '@core';
+declare module '@components';
--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -2,5 +2,5 @@ import 'normalize.css';
 import './index.scss';
 import App from './pages/app';
 
-const app = new App();
+const app = new App('div', '');
 app.run();
