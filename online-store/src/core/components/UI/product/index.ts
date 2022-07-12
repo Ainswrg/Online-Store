432622faf18f1c6f96749f5381ea0c70f9e32260
Jaslan <Jaslan.dlnv@gmail.com>
2022-07-12 17:17:31 +0500

    feat: add buttons and style on product

--- online-store/src/core/components/UI/product/index.ts
+++ online-store/src/core/components/UI/product/index.ts
@@ -9,11 +9,22 @@ class Product extends Component {
     this.cart = cart;
   }
 
-  generateButton(className: string, content: string): HTMLButtonElement {
+  generateButton(className: string, content: string, counter?: number): HTMLDivElement {
+    const btnWrapper = document.createElement('div');
     const button = document.createElement('button');
-    button.classList.add(className);
-    button.textContent = content;
-    return button;
+    btnWrapper.classList.add(className);
+
+    const span = document.createElement('span');
+    span.textContent = content;
+    button.append(span);
+    if (Number(counter) >= 0) {
+      const counterSpan = document.createElement('i');
+      counterSpan.classList.add('product__counter');
+      counterSpan.textContent = `(${counter})`;
+      button.append(counterSpan);
+    }
+    btnWrapper.append(button);
+    return btnWrapper;
   }
 
   generateProduct(): void {
@@ -23,17 +34,21 @@ class Product extends Component {
 
     if (this.cart.title) {
       template += `<div class="product__img">`;
-      template += `<img src="${img}">`;
+      template += `<img src="${img}" alt=${this.cart.img}>`;
       template += `</div>`;
       template += `<div class="product__info">`;
       template += `<h3 class="product__title">${this.cart.title}</h3>`;
-      template += `<p class="product__price">price: ${this.cart.price}$</p>`;
+      template += `<p class="product__price">$${this.cart.price}</p>`;
       template += `<p class="product__quantity">quantity: ${this.cart.quantity}</p>`;
-      template += `<p class="product__writer">writer: ${this.cart.writer}</p>`;
+      template += `<p class="product__writer">${this.cart.writer}</p>`;
+      template += `<ul class="product__genres">genres: ${this.cart.genres
+        .map((genre) => `<li>${genre}</li>`)
+        .join('')}</ul>`;
+      template += `<p class="product__category">[${this.cart.category.join(', ')}]</p>`;
       template += `<p class="product__publishDate">${this.cart.publishDate}</p>`;
       template += `</div>`;
     }
-    const buttonAdd = this.generateButton('product__button', 'Add to cart');
+    const buttonAdd = this.generateButton('product__button', 'Add to cart', 0);
     const buttonDel = this.generateButton('product__button', 'Delete from cart');
 
     this.container.innerHTML = template;
